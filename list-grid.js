b21f396691407ab66f2bed7824564407f88e257c
Jarraxus <93752424+Jarraxus@users.noreply.github.com>
2021-12-15 18:28:47 -0500

    Delete list-grid.js

--- list-grid.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("sList").addEventListener("click", function(){
-      document.getElementById("lgDemo").classList.remove("grid");
-    });
-    document.getElementById("sGrid").addEventListener("click", function(){
-      document.getElementById("lgDemo").classList.add("grid");
-    });
-  });
